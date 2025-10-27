e49190e44eabb20014f651663bda0afb5cc49dbf
PiGeKo <piotrgkonkol@gmail.com>
2025-10-27 04:22:47 +0000

    Added the classes and a plan
    added AuthUi and AuthService and moved loadUser to AuthService as that should be the responsibility of AuthService as the processor of my section of the app

---
+++ src/main/java/com/mycompany/aquamind/auth/AuthUi.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aquamind.auth;
+
+/*
+Author Piotr Konkol
+*/
+/* things  to implement
+showMainMenu (Start)
+showLoginForm (User login)
+showHints (things in boxes, suggestions etc.)
+if pressed otherwise
+    showRegistrationForm( user account creation)
+onSuccessOrError (failed to login please enter xyz/ successful login welcom f"user")
+passLoginInfo ( give info to other class(AuthService or user))
+*/
+
+
+public class AuthUi {
+    
+}
--- src/main/java/com/mycompany/aquamind/user/user.java
+++ src/main/java/com/mycompany/aquamind/user/user.java
@@ -34,7 +34,5 @@ public class user {
         return email;
     }
 
-    public static user loadUser(String username) {
-// return user data
-    }
+
 }
