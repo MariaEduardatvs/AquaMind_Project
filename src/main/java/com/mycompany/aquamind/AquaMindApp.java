0723f2eed76828dcb4b921462634cbd1e4e4c8aa
PiGeKo <piotrgkonkol@gmail.com>
2025-11-15 05:56:18 +0000

    AuthUi/working option?
    My main part for this checkpoint, hopefully it works and doesn't cause issues, if anything arises we can revert and repair. All-nighter, o7

--- src/main/java/com/mycompany/aquamind/AquaMindApp.java
+++ src/main/java/com/mycompany/aquamind/AquaMindApp.java
@@ -6,26 +6,11 @@ package com.mycompany.aquamind;
 
 /**
  *
- * @author User
+ * @author Team Yellow
  */
 public class AquaMindApp {
 
     public static void main(String[] args) {
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        new com.mycompany.aquamind.auth.AuthUi().setVisible(true);
     }
 }
--- src/main/java/com/mycompany/aquamind/auth/AuthService.java
+++ src/main/java/com/mycompany/aquamind/auth/AuthService.java
@@ -2,8 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.aquamind.auth;
-
 /*
 Author Piotr Konkol
 */
@@ -14,7 +12,32 @@ passwordHash
 userExistence : bool
 loadUser
 */ 
+// just an early prototype to fit Checkpt 2
+package com.mycompany.aquamind.auth;
+import com.mycompany.aquamind.user.user;
 
 public class AuthService {
 
+    private user currentUser;  // store whoever is logged in
+    public boolean login(String username, String password) {
+    // no need for logic yet, will just always be correct i think
+        currentUser = new user(username, "placeholder@mail.ie", password);
+        return true;
+    }
+
+ 
+    public boolean register(String username, String password, String email) {
+        // Once again no logic is actually required yet
+        currentUser = new user(username, email, password);
+        return true; 
+    }
+
+    public void setGuest() {
+        System.out.println("AUTH SERVICE: setGuest() called");
+        currentUser = new user("Guest", "none", "none");
+    }
+
+    public user getCurrentUser() {
+        return currentUser;
+    }
 }
