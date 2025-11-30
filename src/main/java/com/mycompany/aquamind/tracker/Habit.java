4ff0168655203f270feb96d0974d382d7d277ce7
Kripa Shrestha <x24141356@student.ncirl.ie>
2025-11-30 01:10:14 +0000

    Habit and Calculator code

--- src/main/java/com/mycompany/aquamind/tracker/Habit.java
+++ src/main/java/com/mycompany/aquamind/tracker/Habit.java
@@ -9,5 +9,27 @@ package com.mycompany.aquamind.tracker;
  * @author Kripa Shrestha
  */
 public class Habit {
+    private String name;
+    private boolean completed;
     
+    //Constructor
+    public Habit(String name, boolean completed){
+        this.name = name;
+        this.completed=completed;
+    }
+    
+    public String getName(){
+      return name;
+    }
+    
+    //return if true habit is completes, else false
+    public boolean isCompleted(){
+        return completed;
+    }
+    
+    //Toggles the habit completion tick box
+    public void toggleCompleted(){
+        this.completed = !this.completed;
+    }
+
 }
--- src/main/java/com/mycompany/aquamind/tracker/WaterUseCalculator.java
+++ src/main/java/com/mycompany/aquamind/tracker/WaterUseCalculator.java
@@ -9,15 +9,35 @@ package com.mycompany.aquamind.tracker;
  * @author Kripa Shrestha
  */
 public class WaterUseCalculator {
+    //Data members
     private double showerMinutes;
     private int laundryLoads;
     private double sinkMinutes;
     
-    public void setShowerMinutes(double showerMinutes){this.showerMinutes = showerMinutes;}
-    public void setLaundryLoads(int laundryLoads){this.laundryLoads = laundryLoads;}
-    public void setSinkMinutes(double sinkMinutes){this.sinkMinutes = sinkMinutes;}
+    //Setters
+    public void setShowerMinutes(double showerMinutes){
+        this.showerMinutes = showerMinutes;
+    }
+    public void setLaundryLoads(int laundryLoads){
+        this.laundryLoads = laundryLoads;
+    }
+    public void setSinkMinutes(double sinkMinutes){
+        this.sinkMinutes = sinkMinutes;
+    }
     
+    //Calculates the approx amount of water used
     public double calculateUse(){
-        return (showerMinutes * 9)+ (laundryLoads * 50) + (sinkMinutes * 6);
+        double showerUse = showerMinutes*9; //9 litres per minute
+        double laundryUse = laundryLoads*70; //70 litres per minute
+        double sinkUse = sinkMinutes*6; //6 litres per minute
+        
+        return showerUse+laundryUse +sinkUse;
+    }
+    
+    //Resets all input values to zero
+    public void reset(){
+        showerMinutes = 0;
+        laundryLoads = 0;
+        sinkMinutes = 0;
     }
 }
